c9758116690ad1921fe90ab19bc7e09387d5ee6b
Uco <mmdsds@u.sogang.ac.kr>
2019-02-28 13:09:29 +0900

    bubble sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,12 @@
+# Bubble Sort
+def bubble_sort(List):
+    for loop in range(len(List)-1):
+        for i in range(len(List)-loop-1):
+            # 인접한 원소가 작으면 교환한다.
+            if List[i] > List[i+1]:
+                List[i],List[i+1] = List[i+1],List[i]
+    print(List)
+
+# example
+# bubble_sort([3,2,4,5,1])
+# result : [1,2,3,4,5]
